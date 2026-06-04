f95595f7a0373385f457822ab49f487a70406cd0
betomercado2604-png <betomercado2604@gmail.com>
2026-06-03 23:39:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const contenedor = document.getElementById("noticias");
 
-function cargarNoticias(){
+function cargarNoticias() {
 
     contenedor.innerHTML = "<h3>Cargando noticias...</h3>";
 
-    fetch("https://gnews.io/api/v4/top-headlines?lang=es&country=mx&max=10&apikey=7855ae2b27f11bd7d233e2e3db59d482")
+    fetch("https://api.spaceflightnewsapi.net/v4/articles/?limit=10")
 
     .then(respuesta => respuesta.json())
 
@@ -12,34 +12,27 @@ function cargarNoticias(){
 
         contenedor.innerHTML = "";
 
-        datos.articles.forEach(noticia => {
+        datos.results.forEach(noticia => {
 
             contenedor.innerHTML += `
-
             <div class="noticia">
-
                 <h2>${noticia.title}</h2>
-
-                <p>${noticia.description}</p>
-
+                <p>${noticia.summary}</p>
                 <a href="${noticia.url}" target="_blank">
                     Leer noticia completa
                 </a>
-
             </div>
-
             `;
-
         });
 
     })
 
     .catch(error => {
 
+        console.error(error);
+
         contenedor.innerHTML =
         "<h3>Error al cargar noticias.</h3>";
-
-        console.log(error);
 
     });
 
